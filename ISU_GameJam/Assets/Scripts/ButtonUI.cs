db6bc312e0f7a91ae5b110e1b3188bd4453d3ade
Arnold Krajewski <arnoldkrajewski@aol.com>
2024-12-31 03:15:59 -0600

    Added start of UI

---
+++ ISU_GameJam/Assets/Scripts/ButtonUI.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ButtonUI : MonoBehaviour
+{
+    private UIDocument _buttonDocument;
+    private Button _uiButton;
+    private void Awake()
+    {
+        _buttonDocument = GetComponent<UIDocument>();
+        if (_buttonDocument == null)
+        {
+            Debug.LogError("UIDocument component not found on GameObject.");
+            return;
+        }
+        _uiButton = _buttonDocument.rootVisualElement.Q<Button>("TextButton");
+        if (_uiButton == null)
+        {
+            Debug.LogError("Button with the name 'TextButton' not found.");
+            return;
+        }
+        _uiButton.RegisterCallback<ClickEvent>(OnPlayGameClick);
+    }
+    private void OnDisable()
+    {
+        if (_uiButton != null)
+        {
+            _uiButton.UnregisterCallback<ClickEvent>(OnPlayGameClick);
+        }
+    }
+    private void OnPlayGameClick(ClickEvent evt)
+    {
+        Debug.Log("You pressed the Start button");
+    }
+}
